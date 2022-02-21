33fa7a54b3e9d3a7645e752921a3f6da6bdb6fc6
dima12345678qaz <dmitrora93@gmail.com>
2022-02-21 17:33:36 +0200

    add home work

---
+++ script.js
@@ -0,0 +1,65 @@
+//task-3
+// let user='Ivan';
+// console.log(user);
+// let userAge=30;
+// console.log(userAge);
+// let test=userAge;
+// console.log(test);
+
+
+//task-4
+// const obj={
+//     name:'Dima',
+//     age:29,
+//     'is Married':true,
+//     skill:null,
+//     hobby:undefined,
+    
+// };
+// console.log(obj)
+
+
+//task-5
+// let isAdult=confirm('Вам є 18 років ?');
+// console.log(isAdult);
+
+
+
+
+//task-6
+// let username='Dmytro';
+// console.log(typeof(username));
+// let secondName='Riaboshapka';
+// console.log(typeof(secondName));
+// let croup='Lv-678.JS.CORE';
+// console.log(typeof(croup));
+// let yearOfBirth=1993;
+// console.log(typeof(yearOfBirth));
+// let married=true;
+// console.log(typeof(married));
+// let language;
+// console.log(typeof(language));
+// let car=null;
+// console.log(typeof(car));
+
+
+
+
+//task-7
+// let userLogin=prompt('Введіть логін')
+// alert(`Привіт ${userLogin}`);
+// let userMail=prompt('Введіть ваш емейл');
+// alert(`Ваш емейл ${userMail}`);
+// let userPassword=prompt('Введіть пароль')
+// alert(`Ваш пароль ${userPassword}`);
+
+
+
+
+//task-8
+// const secondsInHour = 60 * 60;
+// alert(secondsInHour);
+// const secondsInDay = secondsInHour * 24;
+// alert(secondsInDay);
+// const secondsInMonth = secondsInDay * 30;
+// alert(secondsInMonth);
